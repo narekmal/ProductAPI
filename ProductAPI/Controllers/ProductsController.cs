2c0c732415f123ae2246d22f3a9e5ee398e94a73
Narek <narek@yeschef.com>
2023-04-29 20:52:07 +0400

    Added GetProductById to ProductsController

--- ProductAPI/Controllers/ProductsController.cs
+++ ProductAPI/Controllers/ProductsController.cs
@@ -28,5 +28,16 @@ namespace ProductAPI.Controllers
 
             return Ok(products);
         }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProductById(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            if (product == null)
+                return NotFound();
+
+            return Ok(product);
+        }
     }
 }
