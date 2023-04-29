c86704822c994e61e4035c1886b1829eaa7afab8
Narek <narek@yeschef.com>
2023-04-29 21:33:07 +0400

    Added AddProduct to ProductsController

--- ProductAPI/Controllers/ProductsController.cs
+++ ProductAPI/Controllers/ProductsController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using ProductAPI.Models;
 
 namespace ProductAPI.Controllers
 {
@@ -38,6 +39,23 @@ namespace ProductAPI.Controllers
                 return NotFound();
 
             return Ok(product);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> AddProduct([FromBody] Product newProduct)
+        {
+            var existingProduct = await _context.Products.FirstOrDefaultAsync(p => p.Name == newProduct.Name);
+
+            if (existingProduct != null)
+            {
+                return Conflict("A product with the same name already exists.");
+            }
+
+            _context.Products.Add(newProduct);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetProductById), new { id = newProduct.Id }, newProduct);
         }
     }
 }
